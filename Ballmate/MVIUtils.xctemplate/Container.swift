13d8ab106eb0489a7baa2a7bdcfe0d486fee33ca
HenryVoid <azicplus@naver.com>
2024-02-03 22:15:49 +0900

    Ballmate.MVI Utils Template 추가

---
+++ Ballmate/MVIUtils.xctemplate/Container.swift
@@ -0,0 +1,33 @@
+//___FILEHEADER___
+
+import Combine
+import Foundation
+
+final class Container<Intent, State>: ObservableObject {
+    
+    // MARK: Lifecycle
+    
+    init(
+        intent: Intent,
+        state: State,
+        modelChangePublisher: ObjectWillChangePublisher)
+    {
+        self.intent = intent
+        self.state = state
+        
+        modelChangePublisher
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: objectWillChange.send)
+            .store(in: &cancellable)
+    }
+    
+    // MARK: Internal
+    
+    let intent: Intent
+    let state: State
+    
+    // MARK: Private
+    
+    private var cancellable: Set<AnyCancellable> = []
+    
+}
---
+++ Ballmate/MVIUtils.xctemplate/IntentType.swift
@@ -0,0 +1,40 @@
+//___FILEHEADER___
+
+import Foundation
+import Combine
+
+// MARK: - IntentType
+
+protocol IntentType: AnyObject {
+    associatedtype ViewAction
+    associatedtype State
+    
+    var state: State { get set }
+    var cancellable: Set<AnyCancellable> { get set }
+    
+    func send(action: ViewAction)
+    func send(action: ViewAction, viewEffect: (() -> Void)?)
+    func mutate(action: ViewAction, viewEffect: (() -> Void)?)
+}
+
+extension IntentType {
+    
+    func send(action: ViewAction) {
+        mutate(action: action, viewEffect: .none)
+    }
+    
+    func send(action: ViewAction, viewEffect: (() -> Void)?) {
+        mutate(action: action, viewEffect: viewEffect)
+    }
+}
+
+// MARK: - IntentBindingType
+
+protocol IntentBindingType {
+    associatedtype IntentType
+    associatedtype State
+    
+    var container: Container<IntentType, State> { get }
+    var intent: IntentType { get }
+    var state: State { get }
+}
